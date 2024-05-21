613357be7ec6c62023e774eddc73b8ee0ef562b4
Danilo Pezo <danilo_97pem@hotmail.com>
2024-05-20 21:45:43 -0300

    Implementando o componente tickets

--- src/app/tickets/tickets-routing.module.ts
+++ src/app/tickets/tickets-routing.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { MenuComponent } from './menu/menu.component';
 import { ConteudoComponent } from './conteudo/conteudo.component';
 
 const routes: Routes = [{ path: '', component: ConteudoComponent }];
--- src/app/tickets/tickets.module.ts
+++ src/app/tickets/tickets.module.ts
@@ -7,15 +7,31 @@ import { ConteudoComponent } from './conteudo/conteudo.component';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatListModule } from '@angular/material/list';
 import { MatExpansionModule } from '@angular/material/expansion';
+import { TicketsComponent } from './tickets/tickets.component';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import {
+  MatTooltipModule,
+  matTooltipAnimations,
+} from '@angular/material/tooltip';
 
 @NgModule({
-  declarations: [MenuComponent, ConteudoComponent],
+  declarations: [MenuComponent, ConteudoComponent, TicketsComponent],
   imports: [
     CommonModule,
     TicketsRoutingModule,
     MatSidenavModule,
     MatListModule,
     MatExpansionModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatCardModule,
+    MatIconModule,
+    MatButtonModule,
+    MatTooltipModule,
   ],
 })
 export class TicketsModule {}
