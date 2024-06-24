5e4ba7b78275593ee8a2736fe53d724f3de1f087
Danilo Pezo <danilo_97pem@hotmail.com>
2024-06-24 14:14:46 -0300

    Adicionado atributos para o ticket: ID, data, hora, status, solucao, categoria, tag

--- src/app/tickets/tickets/details-ticket/details-ticket.component.ts
+++ src/app/tickets/tickets/details-ticket/details-ticket.component.ts
@@ -3,8 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-details-ticket',
   templateUrl: './details-ticket.component.html',
-  styleUrls: ['./details-ticket.component.scss']
+  styleUrls: ['./details-ticket.component.scss'],
 })
-export class DetailsTicketComponent {
-
-}
+export class DetailsTicketComponent {}
