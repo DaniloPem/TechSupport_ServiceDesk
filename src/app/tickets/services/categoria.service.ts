5222201d86e1705b16d97777c0a937c639649a5d
Danilo Pezo <dpezomendoza@gmail.com>
2025-02-02 12:09:22 -0300

    Implementado método para pegar uma lista de categorias por um lista de Ids

--- src/app/tickets/services/categoria.service.ts
+++ src/app/tickets/services/categoria.service.ts
@@ -3,6 +3,11 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { CategoriasPage } from '../model/categorias-page';
 
+export interface AtributoDto {
+  id: number;
+  nome: string;
+}
+
 @Injectable({
   providedIn: 'root',
 })
@@ -21,5 +26,9 @@ export class CategoriaService {
       .set('page', page)
       .set('pageSize', pageSize);
     return this.httpClient.get<CategoriasPage>(`${this.API}`, { params });
+  }
+
+  getListCategoriasPorIds(ids: number[]): Observable<AtributoDto[]> {
+    return this.httpClient.get<AtributoDto[]>(`${this.API}/${ids}`);
   }
 }
